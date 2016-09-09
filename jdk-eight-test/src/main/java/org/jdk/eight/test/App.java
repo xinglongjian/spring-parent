976617c5b9a359f54b319c2a3498a3912fffd2f0
xinglongjian <xinglongjian@qq.com>
2016-09-09 10:27:56 +0800

    add jdk-eight-test

---
+++ jdk-eight-test/src/main/java/org/jdk/eight/test/App.java
@@ -0,0 +1,13 @@
+package org.jdk.eight.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
